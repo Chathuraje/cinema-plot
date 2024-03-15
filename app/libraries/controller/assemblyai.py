4b60ef1f4830ee5f6ad2e480dab7d9f98f2681cf
Chathura J Ekanayake <chathuraje01@gmail.com>
2024-03-15 19:55:50 +0530

    Refactor transcript generation code in assemblyai.py

--- app/libraries/controller/assemblyai.py
+++ app/libraries/controller/assemblyai.py
@@ -17,31 +17,50 @@ def initialize_assemblyai():
       
         
 def generate_transcript(mp3_file_path, transcript_path):
-    logger.info(f"Generating transcript from audio file")
+    logger.info("Generating transcript from audio file")
     
     transcriber = initialize_assemblyai()
     transcript = transcriber.transcribe(mp3_file_path)
 
     if transcript.status == aai.TranscriptStatus.error:
-        logger.error(f"Error generating transcript")
+        logger.error("Error generating transcript")
     else:
         sentences = transcript.get_sentences() # Getting sentences from the transcript
         for sentence in sentences:
-            sentence_data = []
+            sentences_data = []
             
             for sentence in sentences:
                 start_seconds = round(sentence.start / 1000, 3)
                 end_seconds = round(sentence.end / 1000, 3)
                 duration_seconds = round(end_seconds - start_seconds, 3)
+                
+                words = sentence.words
+                words_data = []
+                total_word_duration = 0
+                
+                for word in words:
+                    word_start_seconds = round(word.start / 1000, 3)
+                    word_end_seconds = round(word.end / 1000, 3)
+                    word_duration_seconds = round(word_end_seconds - word_start_seconds, 3)
                     
-                word_data = {
+                    word_data = {
+                        "start": word_start_seconds,
+                        "end": word_end_seconds,
+                        "duration": word_duration_seconds,
+                        "text": word.text
+                    }
+                    words_data.append(word_data)
+                
+                # Add total word duration for the sentence
+                data = {
                     "start": start_seconds,
                     "end": end_seconds,
                     "duration": duration_seconds,
-                    "text": sentence.text
+                    "text": sentence.text,
+                    "words": words_data,
                 }
                     
-                sentence_data.append(word_data)
+                sentences_data.append(data)
             
             with open(transcript_path, "w") as json_file:
-                json.dump(sentence_data, json_file, indent=4)
+                json.dump(sentences_data, json_file, indent=4)
